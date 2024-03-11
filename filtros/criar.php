89d946bf673e646380f5934d64c577d15611014d
studious <159821950+Mpbooks@users.noreply.github.com>
2024-03-11 07:18:03 -0300

    Add files via upload

--- filtros/criar.php
+++ filtros/criar.php
@@ -3,11 +3,12 @@
 ob_start();
 require('../../sheep_core/config.php');
 
+
 $produto= filter_input_array(INPUT_POST, FILTER_DEFAULT);
 if(isset($produto['criarProduto'])){
     unset($produto['criarProduto']);
   $produto['capa']= ($_FILES['capa']['tmp_name'] ? $_FILES['capa'] : null);
-    
+  
   $salvar = new Produtos();
   $salvar->CriarProduto($produto);
 
@@ -16,7 +17,10 @@ if(isset($produto['criarProduto'])){
   }else{
     header("Location:". HOME."/painel/index.php?erro=true");
   }
+  
 }
 
 
 ?>
+
+
